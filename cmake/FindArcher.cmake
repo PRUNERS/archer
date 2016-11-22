2d4730c4b3648d7152f4151acb11d9e627add36a
Simone Atzeni <atzeni1@llnl.gov>
2016-11-22 12:56:23 -0800

    Fixed cmake and cmake scripts

--- cmake/FindArcher.cmake
+++ cmake/FindArcher.cmake
@@ -3,9 +3,8 @@ include(FindPackageHandleStandardArgs)
 set(ARCHER_LIB LLVMArcher.so)
 find_path(ARCHER_LIB_PATH
   NAMES LLVMArcher.so
-  HINTS ${CMAKE_CURRENT_BINARY_DIR}/lib ${LLVM_ROOT}/lib
+  HINTS ${CMAKE_BINARY_DIR}/src ${CMAKE_BINARY_DIR}/lib ${LLVM_ROOT}/lib ${CMAKE_INSTALL_PREFIX}/lib
   )
 find_path(ARCHER_LIB_PATH LLVMArcher.so)
 
 find_package_handle_standard_args(ARCHER DEFAULT_MSG ARCHER_LIB_PATH)
-
--- cmake/FindOmptTsan.cmake
+++ cmake/FindOmptTsan.cmake
@@ -3,7 +3,7 @@ include(FindPackageHandleStandardArgs)
 set(OMPT_TSAN_LIB ompt-tsan.so)
 find_path(OMPT_TSAN_LIB_PATH
   NAMES ompt-tsan.so
-  HINTS ${CMAKE_CURRENT_BINARY_DIR}/src ${LLVM_ROOT}/lib
+  HINTS ${CMAKE_BINARY_DIR}/src ${CMAKE_BINARY_DIR}/lib ${LLVM_ROOT}/lib ${CMAKE_INSTALL_PREFIX}/lib
 )
 find_path(OMPT_TSAN_LIB_PATH ompt-tsan.so)
 
