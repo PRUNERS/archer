e251445b82e74e456628b75f708f1d163d5f0f99
Simone Atzeni <simone.at@gmail.com>
2017-01-09 14:54:58 -0700

    Fixing CMake

--- cmake/FindOmp.cmake
+++ cmake/FindOmp.cmake
@@ -1,10 +1,13 @@
 include(FindPackageHandleStandardArgs)
 
 # use an explicitly given ompt path first
+
+MESSAGE(${CMAKE_BINARY_DIR})
+
 FIND_PATH(OMP_INCLUDE_PATH omp.h
             PATHS ${LLVM_ROOT}/include ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/include /usr /usr/local ${CMAKE_BINARY_DIR}/projects/openmp/runtime/src}
             PATH_SUFFIXES include NO_DEFAULT_PATH)
 # if not-found, try again at cmake locations
-#FIND_PATH(OMP_INCLUDE_PATH omp.h)
+FIND_PATH(OMP_INCLUDE_PATH omp.h)
 
 find_package_handle_standard_args(OpenMP DEFAULT_MSG OMP_INCLUDE_PATH )
