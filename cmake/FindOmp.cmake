7ffb9ae946cff12ce864cff2a906e5a4f5d123af
Simone Atzeni <simone.at@gmail.com>
2017-01-09 14:47:59 -0700

    Fixing CMake

--- cmake/FindOmp.cmake
+++ cmake/FindOmp.cmake
@@ -5,6 +5,6 @@ FIND_PATH(OMP_INCLUDE_PATH omp.h
             PATHS ${LLVM_ROOT}/include ${CMAKE_INSTALL_PREFIX}/include /usr /usr/local }
             PATH_SUFFIXES include NO_DEFAULT_PATH)
 # if not-found, try again at cmake locations
-FIND_PATH(OMPT_INCLUDE_PATH omp.h)
+FIND_PATH(OMP_INCLUDE_PATH omp.h)
 
 find_package_handle_standard_args(OpenMP DEFAULT_MSG OMP_INCLUDE_PATH )
